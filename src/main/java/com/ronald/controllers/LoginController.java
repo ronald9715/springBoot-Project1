59c33fc4cd3614b03160b8bfac106d80631759e9
Ronald <ronald.agama@unmsm.edu.pe>
2022-11-11 22:31:28 -0500

    Adding Security

---
+++ src/main/java/com/ronald/controllers/LoginController.java
@@ -0,0 +1,47 @@
+package com.ronald.controllers;
+
+import com.ronald.security.JwtRequest;
+import com.ronald.security.JwtResponse;
+import com.ronald.security.JwtTokenUtil;
+import com.ronald.security.JwtUserDetailsService;
+import org.apache.coyote.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.DisabledException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LoginController {
+    @Autowired
+    private AuthenticationManager authenticationManager;
+    @Autowired
+    private JwtTokenUtil jwtTokenUtil;
+    @Autowired
+    private JwtUserDetailsService userDetailsService;
+
+    @PostMapping("/login")
+    public ResponseEntity<JwtResponse> login(@RequestBody JwtRequest jwtRequest) throws Exception{
+       authenticate(jwtRequest.getUsername(), jwtRequest.getPassword());
+       final UserDetails userDetails = userDetailsService.loadUserByUsername(jwtRequest.getUsername());
+       final String token = jwtTokenUtil.generateToken(userDetails);
+       return ResponseEntity.ok(new JwtResponse(token));
+    }
+
+    private void authenticate (String username, String password) throws Exception{
+        try{
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
+        }catch (DisabledException e){
+            throw new Exception("USER_DISABLED");
+        }catch (BadCredentialsException e){
+            throw  new Exception("INVALID_CREDENTIALS");
+        }
+    }
+}
--- src/main/java/com/ronald/security/WebSecurityConfig.java
+++ src/main/java/com/ronald/security/WebSecurityConfig.java
@@ -1,10 +1,22 @@
 package com.ronald.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 //Clase S7
 @Configuration
@@ -17,4 +29,37 @@ public class WebSecurityConfig {
     private UserDetailsService jwtUserDetailsService;
     @Autowired
     private  JwtRequestFilter jwtRequestFilter;
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public static PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception{
+        auth.userDetailsService(jwtUserDetailsService).passwordEncoder(passwordEncoder());
+    }
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception{
+        httpSecurity
+                .csrf().disable()
+                .authorizeHttpRequests()
+                .antMatchers("/login").permitAll()
+                //.antMatchers("/categories/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint)
+                .and()
+                .formLogin().disable()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        httpSecurity.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+        return httpSecurity.build();
+    }
 }
